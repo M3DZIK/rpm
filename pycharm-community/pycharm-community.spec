M3DZIK/rpm

commit 93c88431ef11ab35eafa5b7311ce3f7cfe08c1dc

Exclude musl archive for Fedora build (#11)
* Exclude musl archive for Fedora build

Make sure to select the correct archive for the Fedora build.

Currently the logic in the specfile selects the musl archive, which
ships `libjvm.so` linked against `libc.musl-x86_64.so.1`, which is not
available on Fedora. That leads to an error when starting PyCharm.

* Rebuild pycharm-community

---------

Co-authored-by: Oskar Karpiński <karpinski.oskar@proton.me>

pycharm-community/pycharm-community.spec
@@ -23,7 +23,7 @@
 
 Name:    pycharm-community
 Version: 2025.2.1.1
-Release: 1%{?dist}
+Release: 2%{?dist}
 Summary: Intelligent Python IDE - Community
 License: Apache-2.0
 URL:     https://www.jetbrains.com/%{appname}/
@@ -80,9 +80,11 @@ target_file_pattern="${idea_target_dir}/${target_file_name}"
 
 %ifarch x86_64
 # Exclude aarch64 from search
-target_file=$(ls ${target_file_pattern} 2>/dev/null | grep -v 'aarch64' | head -n 1)
+# Exclude musl from both searches since libjvm.so in the musl archive
+# is linked to libc.musl-x86_64.so.1, which does ot exist in Fedora.
+target_file=$(ls ${target_file_pattern} 2>/dev/null | grep -E -v 'aarch64|musl' | head -n 1)
 %else
-target_file=$(ls ${target_file_pattern} 2>/dev/null | head -n 1)
+target_file=$(ls ${target_file_pattern} 2>/dev/null | grep -E -v 'musl' head -n 1)
 %endif
 
 mkdir -p "unpacked"

commit 9525afd364f0009853e95085370511b955bece62

Fix typos (missing pipe) in pycharm-community.spec
Introduced by https://github.com/M3DZIK/rpm/pull/11

pycharm-community/pycharm-community.spec
@@ -81,10 +81,10 @@ target_file_pattern="${idea_target_dir}/${target_file_name}"
 %ifarch x86_64
 # Exclude aarch64 from search
 # Exclude musl from both searches since libjvm.so in the musl archive
-# is linked to libc.musl-x86_64.so.1, which does ot exist in Fedora.
+# is linked to libc.musl-x86_64.so.1, which does not exist in Fedora.
 target_file=$(ls ${target_file_pattern} 2>/dev/null | grep -E -v 'aarch64|musl' | head -n 1)
 %else
-target_file=$(ls ${target_file_pattern} 2>/dev/null | grep -E -v 'musl' head -n 1)
+target_file=$(ls ${target_file_pattern} 2>/dev/null | grep -E -v 'musl' | head -n 1)
 %endif
 
 mkdir -p "unpacked"